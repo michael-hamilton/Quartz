99f21c5988ecb9e8a25ae1fe996c63119f64ab44
Michael Hamilton <michaelhamilton626@gmail.com>
2015-11-08 14:35:20 -0500

    Added Grunt
    Added initial Gruntfile and package.json

---
+++ Gruntfile.js
@@ -0,0 +1,8 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.registerTask('default', 'Default.', function() {
+    grunt.log.write('Testing 1, 2, 3... ').ok();
+  });
+
+};
