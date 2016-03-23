b7bb503391d3ec9ef4ad0fb7dc53d0c8e3bc0c31
Michael Hamilton <michaelhamilton626@gmail.com>
2016-03-23 13:43:17 -0400

    Updated Gruntfile and split quartz into base and full versions
    Split Quartz into a base version and a full version. Updated Gruntfile
    to build both of these files and minify them to /dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,8 @@ module.exports = function(grunt) {
 		sass: {
 			dist: {
 				files: {
-                    'dist/css/quartz.css': 'sass/quartz.scss'
+					'dist/css/quartz-base.css': 'sass/quartz.scss',
+                    'dist/css/quartz.css': 'sass/quartz-full.scss'
                 }
 			}
 		},
@@ -15,9 +16,23 @@ module.exports = function(grunt) {
                 ],
 				tasks: ['sass']
 			}
+		},
+		cssmin: {
+			target: {
+				files: [{
+					expand: true,
+					cwd: 'dist/css',
+					src: ['*.css', '!*.min.css'],
+					dest: 'dist/css',
+					ext: '.min.css'
+				}]
+			}
 		}
+
 	});
 	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.registerTask('default',['watch']);
+	grunt.registerTask('build',['sass', 'cssmin']);
 }
